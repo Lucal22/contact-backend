411b5a7ebcea5b373a265f7580f1be5ab6478844
Luis Carlos <lucaldev22@gmail.com>
2023-01-12 12:38:47 -0300

    Tests done.

--- src/use-cases/contact-use-case.test.ts
+++ src/use-cases/contact-use-case.test.ts
@@ -1,16 +1,22 @@
-import { SubmitContactUseCase } from "./contact-use-case";
+import { SubmitContactUseCase, UpdateContactUseCase } from "./contact-use-case";
 
 const createSubmitSpy = jest.fn();
+const createUpdateSpy = jest.fn();
+
 
 const submitContact = new SubmitContactUseCase(
     {create: createSubmitSpy},
+)
+
+const updateContact = new UpdateContactUseCase(
+    {update: createUpdateSpy},
 )
 
 describe('Submit contact',()=>{
     it('should be able to submit the contact',async ()=>{
         
         await expect(submitContact.execute({
-            email: 'test@email.com',
+            email: 'luis@gmail.com',
             name: 'luis',
             phone: '987654321',
     })).resolves.not.toThrow();
@@ -21,10 +27,52 @@ describe('Submit contact',()=>{
     it('should not be able to submit the contact', async ()=>{
         
         await expect(submitContact.execute({
-            email: 'test@email.com',
+            email: 'luis@gmail.com',
+            name: 'luis',
+            phone: '',
+        })).resolves.toThrow()
+
+    })
+
+    it('should not be able to submit the contact', async ()=>{
+    
+        await expect(submitContact.execute({
+            email: 'luis@gmail.com',
+            name: 'luis',
+            phone: '0319876543212345',
+        })).resolves.toThrow()
+    })
+
+})
+
+describe('Update contact',()=>{
+    it('should be able to update the contact',async ()=>{
+        
+        await expect(updateContact.execute({
+            id: 'asoifh1819301fnd112jn12',
             name: 'luis',
             phone: '987654321',
-        })).resolves.toThrow()
+    })).resolves.not.toThrow();
+
+        expect(createSubmitSpy).toHaveBeenCalled();
+    })
+
+    it('should be able to update the contact',async ()=>{
+        
+        await expect(updateContact.execute({
+            id: 'asoifh1819301fnd112jn12',
+            name: 'luis',
+            phone: '',
+    })).resolves.toThrow();
+    })
+
+    it('should be able to update the contact',async ()=>{
+        
+        await expect(updateContact.execute({
+            id: 'asoifh1819301fnd112jn12',
+            name: 'luis',
+            phone: '0319876543212345',
+    })).resolves.toThrow();
     })
 
 })
